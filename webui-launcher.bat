19940a744b4784b26fc1ce2cc52a6c7d8247bd0b
zobinimm <z0bin@163.com>
2024-12-12 16:48:54 +0800

    Fix CUDA memory

--- webui-launcher.bat
+++ webui-launcher.bat
@@ -3,7 +3,7 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--use-cpu all --no-half --precision full --skip-torch-cuda-test --api --nowebui
-set CUDA_VISIBLE_DEVICES=-1
+set COMMANDLINE_ARGS=--use-cpu all --no-half --precision full --skip-torch-cuda-test --api --nowebui --xformers
+set PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:12288
 
 call webui.bat
